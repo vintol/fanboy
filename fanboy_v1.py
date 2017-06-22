277e07a70531cfc8efa553df4d9d17b267c25be6
vintol <vintol@outlook.com>
2017-06-22 18:11:32 +0530

    Legal Notice

--- fanboy_v1.py
+++ fanboy_v1.py
@@ -2,8 +2,12 @@
 # Fansite Image Retriver
 # 03/06/2017
 #
+#   This file is part of fanboy.
+#   fanboy is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   any later version.
 #
-
 
 import urllib.request
 import bs4
